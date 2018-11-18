1eaf8685fdeda3b612c22083edb9cc2404228dcb
calvinweb <38599774+calvinweb@users.noreply.github.com>
2018-11-18 10:38:54 +0800

    Update ____/bph.ino

--- ____/bph.ino
+++ ____/bph.ino
@@ -28,3 +28,15 @@ void red() {
   while (pa != 0);
   return pd
 }
+void green() {
+  pd = 0;
+  pa = 0;
+  digitalWrite(S1, );
+  digitalWrite(S2, 0);
+
+  MsTimer2::set(10, onTimer); //设置中断，每1000ms进入一次中断服务程序 onTimer()
+  MsTimer2::start();
+  while (pa != 0);
+  MsTimer2::stop()
+  return pd
+}
