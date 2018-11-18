56c114f9dc06343b0a2f13db28d37a5d309422bb
calvinweb <38599774+calvinweb@users.noreply.github.com>
2018-11-18 10:13:31 +0800

    Update ____/bph.ino

--- ____/bph.ino
+++ ____/bph.ino
@@ -5,17 +5,19 @@ void setup(){
   pinMode(S3,OUTPUT);
   
 }
-int pa=0
+int pd=0;
+int pa=0;
 void time(){
 
 }
+
 void red(){
-  pd=0
-  pa=0
-  digitalWrite(S1,0)
+  pd=0;
+  pa=0;
+  digitalWrite(S1,0);
   digitalWrite(S2,0);
     
   MsTimer2::set(10, onTimer); //设置中断，每1000ms进入一次中断服务程序 onTimer()
-  MsTimer2::start(); 
+  MsTimer2::start();
   while(pa!=0);
 }
